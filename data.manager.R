8cefe83d67be935527d99a2895931ada8f6fd119
mschnure <mschnur3@jhmi.edu>
2022-02-25 10:06:38 -0500

    MS data manager

--- data.manager.R
+++ data.manager.R
@@ -35,9 +35,55 @@ get.surveillance.data = function(data.manager,
 # sub function for overwrite 2D, 3D, 4D
 
 
-read.surveillance.data = function(dir #where the tables are
+read.surveillance.data = function(dir = 'data/raw_data' #where the tables are
                                   )
 {
+        ## Prevalence
+        sub.dir = file.path(dir,'plhiv')
+        
+        files = list.files(file.path(sub.dir))
+
+        df.prevalence = NULL
+        
+        for(i in 1:length(files)){
+                one.df = read.csv(file.path(sub.dir,files[i]))
+                df.prevalence = rbind(df.prevalence, one.df)   
+        }
+        
+        
+        ## Diagnoses
+        sub.dir = file.path(dir,'new')
+        
+        files = list.files(file.path(sub.dir))
+        
+        df.new = NULL
+        
+        for(i in 1:length(files)){
+                one.df = read.csv(file.path(sub.dir,files[i]))
+                df.new = rbind(df.new, one.df)   
+        }
+        
+        
+        
+        ## Below things are empty - starting to set up 
+        
+        diagnoses = list()
+        
+        diagnoses.total = array()
+        diagnoses$TOTAL = diagnoses.total
+        
+        diagnoses.subgroup = array()
+        diagnoses$SUBGROUP = diagnoses.subgroup
+        
+        diagnoses.subgroup.age = array()
+        diagnoses$SUBGROUP.AGE = diagnoses.subgroup.age
+        
+        diagnoses.subgroup.sex = array()
+        diagnoses$SUBGROUP.SEX = diagnoses.subgroup.sex
+        
+        diagnoses.subgroup.age.sex = array()
+        diagnoses$SUBGROUP.AGE.SEX = diagnoses.subgroup.age.sex
+        
         # sub list for every data type, each element of diagnoses sublist has an array for what we have 
         # (total/year, year/subgroup; year/subgroup/sex; year/subgroup/age; year/subgroup/age/sex)
         
