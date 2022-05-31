7799b0bc6ca6fdcecb56f72bd9635bb09353bd69
mschnure <mschnur3@jhmi.edu>
2022-05-31 15:17:30 -0400

    redid optimization code - 5/26 meeting

--- model/parameter_optim.R
+++ model/parameter_optim.R
@@ -6,64 +6,50 @@ par = c(fertility.multiplier=1,
 
 sumSqMin = function(par,
                     data.manager=DATA.MANAGER) {
-        browser()
-        parameters = create.model.parameters()
         
-        ## sampled parameters set to NA if I don't pass them 
-        parameters = map.model.parameters(parameters,
-                                          sampled.parameters=c(fertility.multiplier=par["fertility.multiplier"],
-                                                               over.80.mortality.multiplier=par["over.80.mortality.multiplier"],
-                                                               hiv.mortality.rates.suppressed=0.01, 
-                                                               hiv.mortality.rates.unsuppressed=0.02,
-                                                               # non.hiv.mortality.rates=0.01, 
-                                                               testing.rates=1.5, 
-                                                               engagement.rates=3,
-                                                               unsuppressed.disengagement.rates=0.2,
-                                                               suppressed.disengagement.rates=0.2,
-                                                               suppression.rates=3,
-                                                               unsuppression.rates=0.1,
-                                                               global.transmission.rate=6, #the average number of infections from one undiagnosed HIV+ person per year 
-                                                               relative.transmission.from.diagnosis=0.33))
+        sim = run.model.for.parameters(variable.parameters=par)
         
-        state.dim.names = list(age=parameters$AGES, 
-                               sex=parameters$SEXES,
-                               subgroup=parameters$SUBGROUPS,
-                               hiv.status=parameters$HIV.STATUS)
+        score.sim(sim)
         
-        initial.state = get.initial.population(year = "1970", 
-                                               data.manager = DATA.MANAGER, 
-                                               model.age.cutoffs = MODEL.AGE.CUTOFFS, 
-                                               ages = parameters$AGES, 
-                                               sexes = parameters$SEXES, 
-                                               seed.to.ages = c(4,5,6), 
-                                               seed.to.sexes = c(1,2), 
-                                               seed.n = 1)
-        
-        # for some reason, running the model here returns NAs for the population 
-        sim = run.model(parameters=parameters,
-                        initial.state=initial.state,
-                        start.year=1970,
-                        end.year=2020,
-                        keep.years=c(1970:2020))
-        
+}
+
+score.sim = function(sim){
         sim.pop = extract.data(sim,
-                               years=2000:2010, 
+                               years=2010:2020, 
                                data.type = "population",
                                keep.dimensions = c("year","age"))
         
         true.pop = get.surveillance.data(data.manager = DATA.MANAGER, 
-                                         years=2000:2010, 
+                                         years=2010:2020, 
                                          data.type = "population",
                                          keep.dimensions = c("year","age"))
         
         # sum of squared errors of the population proportion - I think 
-        sum(((sim.pop/rowSums(sim.pop)) - (true.pop/rowSums(true.pop)))^2)
+        #sum(((sim.pop/rowSums(sim.pop)) - (true.pop/rowSums(true.pop)))^2)
         
         # sum of squared errors of the population itself 
-        # sum((sim.pop - true.pop)^2)
+         log(sum((sim.pop - true.pop)^2))
 }
 
 rv = optim(par = par, fn = sumSqMin)
+
+sim.test.optim=run.model.for.parameters(variable.parameters = rv$par)
+sim.test.manual = run.model.for.parameters(variable.parameters = c(rv$par[1]))
+
+print(simplot(sim.test.optim, 
+              years=c(2010:2020),
+              data.types = "population", facet.by = 'age'))
+
+print(simplot(sim.test.manual, 
+              years=c(2010:2020),
+              data.types = "population", facet.by = 'age'))
+
+# lower score better 
+score.sim(sim.test.manual)
+score.sim(sim.test.optim)
+
+
+
 
 
 
