b010378a2acf9335d0b8e5dcf56827bb7caa5c05
mschnure <mschnur3@jhmi.edu>
2023-01-12 09:53:21 -0500

    update frequency

--- calibration/run_mcmc_chain.R
+++ calibration/run_mcmc_chain.R
@@ -2,4 +2,4 @@ CHAIN = 1 # Todd will change this to 2-4 for other runs
 
 run.mcmc.from.cache(dir="mcmc_cache",
                     chains=CHAIN,
-                    update.frequency = 200)
+                    update.frequency = 500)
