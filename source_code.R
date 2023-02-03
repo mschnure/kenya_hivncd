c4991bf6d4bfeaf4268d6140f73454a198bd6b9f
mschnure <mschnur3@jhmi.edu>
2023-02-03 10:56:48 -0500

    load new data manager

--- source_code.R
+++ source_code.R
@@ -3,7 +3,8 @@ source('model/diffeq.R')
 source('model/age_mappings.R') 
 source('interventions/interventions.R')
 
-load('cached/data.manager.Rdata')
+# load('cached/data.manager.Rdata') - without scaling calibration targets
+load('cached/data.manager_2023-02-03.Rdata') # with scaled calibration targets
 source('calibration/parameter_mappings/south_africa_age_mixing.R')
 source('calibration/parameter_mappings/age_sex_mixing_proportions.R')
 source('calibration/parameter_mappings/age_sex_transmission_multipliers.R')
