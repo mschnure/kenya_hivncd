fa53d4628c84e76b75d6db3bc7c83198a8657722
mschnure <mschnur3@jhmi.edu>
2023-03-29 10:12:15 -0400

    setting cache directory based on file_settings

--- calibration/run_mcmc_chain.R
+++ calibration/run_mcmc_chain.R
@@ -1,6 +1,6 @@
 CHAIN = 4 # Todd will change this to 2-4 for other runs
 
-MCMC.DIR = "R:melissa/mcmcruns"
+source('calibration/file_settings.R')
 
 library(bayesian.simulations)
 library(ggplot2)
@@ -13,6 +13,6 @@ BASE.PARAMETERS=create.model.parameters()
 
 print(qplot(1,1) + ggtitle(paste0("CHAIN ", CHAIN)))
 
-run.mcmc.from.cache(dir=file.path(MCMC.DIR, "mcmc_cache"),
+run.mcmc.from.cache(dir=file.path(MCMC.DIR, CACHE.NAME),
                     chains=CHAIN,
                     update.frequency = 500)
