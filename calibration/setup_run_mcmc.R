03ccafd85d800207004a21032587555a4dc16c03
mschnure <mschnur3@jhmi.edu>
2023-04-06 11:24:26 -0400

    testing out mcmc

--- calibration/setup_run_mcmc.R
+++ calibration/setup_run_mcmc.R
@@ -47,8 +47,9 @@ set.seed(1234)
 # mcmc.21 (3/24) - moved trate.4 spline out to 2040; added cascade improvement end year
 # mcmc.22 (3/27) - removed cascade improvement end year; added proportion.trate.change.by.3.5
 # mcmc.23 (3/29) - added proportion.trate.change.by.3.5 to sampling; trate.4 to trate.3 SD narrowed back to log(2)/2
-# mcmc.24 (3/3_) - narrowed prior on proportion.trate.change.by.3.5; trate.4 to trate.3 SD widened back to log(4)/2
-# mcmc.25 (3/3_ - simultaneous with v24) - median trate.4.to.3 up to 1.25; trate.4 to trate.3 SD narrowed back to log(2)/2
+# mcmc.24 (4/03) - narrowed prior on proportion.trate.change.by.3.5; trate.4 to trate.3 SD widened back to log(4)/2
+# mcmc.25 (4/03 - simultaneous with v24) - median trate.4.to.3 up to 1.25; trate.4 to trate.3 SD narrowed back to log(2)/2
+# mcmc.26 (4/__) - weights after 2018 increased to 4 (fixed data point weighting); put median 4.to.3 back to 1; tightened prop change by 3.5 even more 
 
 # run.mcmc.from.cache() - to resume running if I stop (need the cache directory)
 
@@ -90,7 +91,7 @@ control = create.adaptive.blockwise.metropolis.control(var.names = prior@var.nam
                                                        thin = 5) 
 
 # set starting.values 
-mcmc.23 = run.mcmc.with.cache(control = control,
+mcmc.24 = run.mcmc.with.cache(control = control,
                               n.iter = 10000,
                               starting.values = params.start.values, 
                               update.frequency = 5,
