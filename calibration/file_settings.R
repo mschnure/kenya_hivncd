09f0c0beba04958c5e84dc784f650bc4aa8d4ba2
mschnure <mschnur3@jhmi.edu>
2023-04-27 09:15:09 -0400

    mcmc 31 with stratum-specific penalty (0.3)

--- calibration/file_settings.R
+++ calibration/file_settings.R
@@ -1,4 +1,4 @@
 
-MCMC.VERSION = 30
+MCMC.VERSION = 31
 MCMC.DIR = "R:melissa/mcmcruns"
 CACHE.NAME = paste0("mcmc_cache_",MCMC.VERSION) 
--- calibration/likelihood/likelihood.R
+++ calibration/likelihood/likelihood.R
@@ -147,17 +147,17 @@ create.likelihood = function(data.manager=DATA.MANAGER,
                                                         use.age=T,
                                                         use.age.sex=F)
     
-    awareness.trend.lik = create.likelihood.for.trend(data.type = "awareness",
-                                                      year.1=2025,
-                                                      year.2=2030,
-                                                      probability.of.decrease=.05,
-                                                      use.strata=F)
-    
-    # awareness.stratum.trend.lik = create.likelihood.for.trend(data.type = "awareness",
+    # awareness.trend.lik = create.likelihood.for.trend(data.type = "awareness",
     #                                                   year.1=2025,
     #                                                   year.2=2030,
-    #                                                   probability.of.decrease=.1,
-    #                                                   use.strata=T)
+    #                                                   probability.of.decrease=.05,
+    #                                                   use.strata=F)
+    
+    awareness.stratum.trend.lik = create.likelihood.for.trend(data.type = "awareness",
+                                                      year.1=2025,
+                                                      year.2=2030,
+                                                      probability.of.decrease=.3,
+                                                      use.strata=T)
     
     components = list(incidence=incidence.lik,
                       prevalence=prevalence.lik,
